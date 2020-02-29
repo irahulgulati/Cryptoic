aa83b50f4fcc8852af7b3efdfe85934df73fba36
Rahul <gulati.rg65@gmail.com>
2020-02-29 13:30:20 -0400

    Minor Bug fixes

--- rsa/views.py
+++ rsa/views.py
@@ -4,18 +4,21 @@ from .forms import rsaForm
 from .compute import compute,decrypt
 # Create your views here.
 new = None
+val = None
 def input_values_view(request):
 	result = []
-	global new
+	global new, val
 	form=rsaForm(request.POST or None)
 	if form.is_valid():
 		form2	=form.save(commit=False)
 		if 'encrypt' in request.POST:	
-			encrypt=1
+			encrypt=val=1
 			new= result	=compute(encrypt,form2.p, form2.q, form2.e, form2.m)
-		elif 'decrypt' in request.POST:
+		elif 'decrypt' in request.POST and val == 1:
 			decrypt=2
 			result = compute(decrypt,form2.p, form2.q, form2.e, new)
+		else:
+			result = "Please encrypt before decrypting"
 	context={
 		'form':form,
 		'result':result
